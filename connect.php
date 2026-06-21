19a294db4460eae4cfa980d6cc4ebf0d3910e8f5
nikadilla <nikadillanurnajiha@gmail.com>
2026-06-22 01:53:24 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,7 +4,8 @@ $user = "root";
 $password = "";
 $database = "materncare";
 $conn = mysqli_connect($host, $user, $password, $database);
-if(!$conn){
+if(!$conn)
+{
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
