9ed41073619d6bc3a92b34036b602da905a13ed6
zahirahfarah80-tech <zahirahfarah80@gmail.com>
2026-06-22 19:34:52 +0800

    Update doctorrecord.php

--- doctorrecord.php
+++ doctorrecord.php
@@ -1,41 +1,45 @@
 <?php
 session_start();
-
-$conn = mysqli_connect("localhost","root","","materncare");
-
-$doctor_id = $_SESSION['Doctor_ID'];
-
-$sql = "
-SELECT *
-FROM record
-WHERE Doctor_ID='$doctor_id'
-";
-
-$result = mysqli_query($conn,$sql);
+$conn=mysqli_connect("localhost","root","","materncare");
+$doctor_id=$_SESSION['Doctor_ID'];
+$search="";
+if(isset($_GET['search']))
+{
+$search=$_GET['search'];
+$sql="SELECT * FROM record WHERE Doctor_ID='$doctor_id' AND Name LIKE '%$search%'";
+}
+else
+{
+$sql="SELECT * FROM record WHERE Doctor_ID='$doctor_id'";
+}
+$result=mysqli_query($conn,$sql);
 ?>
 
 <!DOCTYPE html>
 <html>
-
 <head>
-    <title>Patient Record</title>
-    <link rel="stylesheet" href="doctorrecord.css">
+<title>Patient Record</title>
+<link rel="stylesheet" href="doctorrecord.css">
 </head>
 
 <body>
 
 <nav>
-    <div class="logo">
-        <img src="logo.jfif" alt="Logo">
-        <h1>MaternCare</h1>
-    </div>
 
-    <ul>
-        <li><a href="doctorhome.php">Home</a></li>
-        <li><a href="doctorrecord.php">Patient Record</a></li>
-        <li><a href="bookinglist.php">Booking List</a></li>
-        <li><a href="logout.php" class="logout-btn">Sign Out</a></li>
-    </ul>
+<div class="nav-search">
+<form method="GET">
+<input type="text" name="search" placeholder="Search patient name..." value="<?php echo $search;?>">
+<button type="submit">Search</button>
+</form>
+</div>
+
+<ul>
+<li><a href="doctorhome.php">Home</a></li>
+<li><a href="doctorrecord.php">Patient Record</a></li>
+<li><a href="bookinglist.php">Booking List</a></li>
+<li><a href="logout.php" class="logout-btn">Sign Out</a></li>
+</ul>
+
 </nav>
 
 <h1>Patient Record</h1>
@@ -45,32 +49,34 @@ $result = mysqli_query($conn,$sql);
 <table>
 
 <tr>
-    <th>Record Ref</th>
-    <th>Booking Ref</th>
-    <th>Name</th>
-    <th>Patient Details</th>
-    <th>Health Assessment</th>
+<th>Record Ref</th>
+<th>Booking Ref</th>
+<th>Name</th>
+<th>Patient Details</th>
+<th>Health Assessment</th>
 </tr>
 
-<?php while($row = mysqli_fetch_assoc($result)) { ?>
+<?php while($row=mysqli_fetch_assoc($result)){ ?>
 
 <tr>
 
-    <td><?php echo $row['Record_REF']; ?></td>
-    <td><?php echo $row['Booking_REF']; ?></td>
-    <td><?php echo $row['Name']; ?></td>
+<td><?php echo $row['Record_REF'];?></td>
 
-    <td>
-        <a href="patientdetails.php?record_ref=<?php echo $row['Record_REF']; ?>" class="view-btn">
-            View
-        </a>
-    </td>
+<td><?php echo $row['Booking_REF'];?></td>
 
-    <td>
-        <a href="healthassessment.php?record_ref=<?php echo $row['Record_REF']; ?>" class="assessment-btn">
-            Assessment
-        </a>
-    </td>
+<td><?php echo $row['Name'];?></td>
+
+<td>
+<a href="patientdetails.php?record_ref=<?php echo $row['Record_REF'];?>" class="view-btn">
+View
+</a>
+</td>
+
+<td>
+<a href="healthassessment.php?record_ref=<?php echo $row['Record_REF'];?>" class="assessment-btn">
+Assessment
+</a>
+</td>
 
 </tr>
 
