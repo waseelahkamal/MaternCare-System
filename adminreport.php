8693de9e1075e8774a23b15920585a79400970c2
waseelahkamal <waseelahkamal@gmail.com>
2026-06-22 22:40:42 +0800

    Update adminreport.php

--- adminreport.php
+++ adminreport.php
@@ -1,11 +1,7 @@
 <?php
 $conn=mysqli_connect("localhost","root","","materncare");
 
-$sql="SELECT MONTHNAME(appointment_date) AS Month,
-COUNT(*) AS Total
-FROM bookings
-GROUP BY MONTH(appointment_date)
-ORDER BY MONTH(appointment_date)";
+$sql="SELECT MONTHNAME(appointment_date) AS Month,COUNT(*) AS Total FROM booking GROUP BY MONTH(appointment_date) ORDER BY MONTH(appointment_date)";
 
 $result=mysqli_query($conn,$sql);
 
@@ -15,9 +11,9 @@ $data=[];
 
 while($row=mysqli_fetch_assoc($result))
 {
-    $month[]=$row['Month'];
-    $total[]=$row['Total'];
-    $data[]=$row;
+$month[]=$row['Month'];
+$total[]=$row['Total'];
+$data[]=$row;
 }
 ?>
 
@@ -43,14 +39,17 @@ while($row=mysqli_fetch_assoc($result))
         <img src="logo.jfif" alt="Logo">
         <h1>MaternCare</h1>
     </div>
-
+    
     <ul>
         <li><a href="adminhome.php">Home</a></li>
         <li><a href="adminreport.php">Report</a></li>
         <li><a href="doctorlist.php">Doctor List</a></li>
         <li><a href="adminrecord.php">Record</a></li>
-        <li><a href="logout.php" class="logout-btn">Sign Out</a></li>
+        <li><a href="logout.php"class="logout-btn">Sign Out</a>
+    </li>
     </ul>
+
+</nav>
 
 </nav>
 
@@ -69,9 +68,9 @@ while($row=mysqli_fetch_assoc($result))
 
 <tr>
 
-<td><?php echo $row['Month']; ?></td>
+<td><?php echo $row['Month'];?></td>
 
-<td><?php echo $row['Total']; ?></td>
+<td><?php echo $row['Total'];?></td>
 
 </tr>
 
@@ -95,15 +94,16 @@ document.getElementById("myChart"),
 type:"bar",
 
 data:{
-labels:<?php echo json_encode($month); ?>,
+labels:<?php echo json_encode($month);?>,
 
 datasets:[
 {
 label:"Appointment",
-data:<?php echo json_encode($total); ?>
+data:<?php echo json_encode($total);?>
 }
 ]
 }
+
 }
 );
 
