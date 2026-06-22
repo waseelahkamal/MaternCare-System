c823259753aca50bdcf849e8ea3da74bf3450700
amirahzulkifli06-max <amirahzulkifli06@gmail.com>
2026-06-22 22:08:40 +0800

    Update bookinglist.php

--- bookinglist.php
+++ bookinglist.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 
@@ -28,7 +29,19 @@ if(isset($_GET['approve']))
 /* =========================
    REJECT BOOKING
 ========================= */
+if(isset($_GET['reject']))
+{
+    $booking_ref = $_GET['reject'];
 
+    mysqli_query($conn,"
+    UPDATE booking
+    SET Status='Rejected'
+    WHERE Booking_REF='$booking_ref'
+    ");
+
+    header("Location: bookinglist.php");
+    exit();
+}
 
 /* =========================
    CREATE RECORD
