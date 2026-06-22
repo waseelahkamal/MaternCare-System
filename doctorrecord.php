7d1024c30ec65790179b6d5803a48390d86c08ef
waseelahkamal <waseelahkamal@gmail.com>
2026-06-22 22:38:50 +0800

    Update doctorrecord.php

--- doctorrecord.php
+++ doctorrecord.php
@@ -1,41 +1,18 @@
 <?php
 session_start();
-
-$conn = mysqli_connect("localhost","root","","materncare");
-
-$doctor_id = $_SESSION['Doctor_ID'];
-
-$search = "";
-
+$conn=mysqli_connect("localhost","root","","materncare");
+$doctor_id=$_SESSION['Doctor_ID'];
+$search="";
 if(isset($_GET['search']))
 {
-    $search = trim($_GET['search']);
-
-    $sql = "SELECT * FROM record
-            WHERE Doctor_ID='$doctor_id'
-            AND Name LIKE '%$search%'";
-
-    $result = mysqli_query($conn,$sql);
-
-    /* EXTRA VALIDATION */
-    if(mysqli_num_rows($result) === 0)
-    {
-        $message = "No patient record found.";
-    }
+$search=$_GET['search'];
+$sql="SELECT * FROM record WHERE Doctor_ID='$doctor_id' AND Name LIKE '%$search%'";
 }
 else
 {
-    $sql = "SELECT * FROM record
-            WHERE Doctor_ID='$doctor_id'";
-
-    $result = mysqli_query($conn,$sql);
+$sql="SELECT * FROM record WHERE Doctor_ID='$doctor_id'";
 }
-
-/* SECURITY CHECK */
-if($doctor_id == "")
-{
-    die("Unauthorised Access");
-}
+$result=mysqli_query($conn,$sql);
 ?>
 
 <!DOCTYPE html>
@@ -51,16 +28,8 @@ if($doctor_id == "")
 
 <div class="nav-search">
 <form method="GET">
-
-<input
-type="text"
-name="search"
-placeholder="Search patient name..."
-value="<?php echo htmlspecialchars($search); ?>"
->
-
+<input type="text" name="search" placeholder="Search patient name..." value="<?php echo $search;?>">
 <button type="submit">Search</button>
-
 </form>
 </div>
 
@@ -75,13 +44,6 @@ value="<?php echo htmlspecialchars($search); ?>"
 
 <h1>Patient Record</h1>
 
-<?php
-if(isset($message))
-{
-    echo "<p>$message</p>";
-}
-?>
-
 <div class="table-container">
 
 <table>
@@ -94,24 +56,24 @@ if(isset($message))
 <th>Health Assessment</th>
 </tr>
 
-<?php while($row = mysqli_fetch_assoc($result)){ ?>
+<?php while($row=mysqli_fetch_assoc($result)){ ?>
 
 <tr>
 
-<td><?php echo $row['Record_REF']; ?></td>
+<td><?php echo $row['Record_REF'];?></td>
 
-<td><?php echo $row['Booking_REF']; ?></td>
+<td><?php echo $row['Booking_REF'];?></td>
 
-<td><?php echo strtoupper($row['Name']); ?></td>
+<td><?php echo $row['Name'];?></td>
 
 <td>
-<a href="patientdetails.php?record_ref=<?php echo $row['Record_REF']; ?>" class="view-btn">
+<a href="patientdetails.php?record_ref=<?php echo $row['Record_REF'];?>" class="view-btn">
 View
 </a>
 </td>
 
 <td>
-<a href="healthassessment.php?record_ref=<?php echo $row['Record_REF']; ?>" class="assessment-btn">
+<a href="healthassessment.php?record_ref=<?php echo $row['Record_REF'];?>" class="assessment-btn">
 Assessment
 </a>
 </td>
