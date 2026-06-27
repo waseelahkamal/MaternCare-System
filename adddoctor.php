6af7c668454229d93e2cfbb4d3a49e5e4778959b
zahirahfarah80-tech <zahirahfarah80@gmail.com>
2026-06-27 21:07:13 +0800

    Update adddoctor.php

--- adddoctor.php
+++ adddoctor.php
@@ -2,40 +2,89 @@
 
 include("connect.php");
 
-$message = "";
+$message="";
+
+$get=mysqli_query(
+$conn,
+"SELECT Doctor_ID
+FROM doctors
+ORDER BY Doctor_ID DESC
+LIMIT 1"
+);
+
+if(mysqli_num_rows($get)>0)
+{
+$row=mysqli_fetch_assoc($get);
+
+$last_id=$row['Doctor_ID'];
+
+$number=(int)substr($last_id,1);
+
+$number++;
+
+$doctor_id="D".str_pad(
+$number,
+3,
+"0",
+STR_PAD_LEFT
+);
+}
+else
+{
+$doctor_id="D001";
+}
+
 
 if(isset($_POST['add']))
 {
-    $doctor_id = $_POST['doctor_id'];
-    $name = $_POST['name'];
-    $specialization = $_POST['specialization'];
-    $email = $_POST['email'];
 
-    $sql = "INSERT INTO doctors
-            (Doctor_ID, Name, Specialization, Email)
-            VALUES
-            ('$doctor_id','$name','$specialization','$email')";
+$name=$_POST['name'];
+$specialization=$_POST['specialization'];
+$email=$_POST['email'];
 
-    if(mysqli_query($conn,$sql))
-    {
-        $message = "Doctor Added Successfully!";
-    }
-    else
-    {
-        $message = "Failed to Add Doctor!";
-    }
+$sql="INSERT INTO doctors
+(
+Doctor_ID,
+Name,
+Specialization,
+Email
+)
+
+VALUES
+(
+'$doctor_id',
+'$name',
+'$specialization',
+'$email'
+)";
+
+if(mysqli_query($conn,$sql))
+{
+
+$message="Doctor Added Successfully!";
+
+header("Refresh:1");
+
+}
+else
+{
+$message="Failed to Add Doctor!";
+}
+
 }
 
 ?>
 
 <!DOCTYPE html>
+
 <html>
 
 <head>
 
-    <title>Add Doctor</title>
+<title>Add Doctor</title>
 
-    <link rel="stylesheet" href="adddoctorstyle.css">
+<link rel="stylesheet"
+href="adddoctorstyle.css">
 
 </head>
 
@@ -43,19 +92,19 @@ if(isset($_POST['add']))
 
 <nav>
 
-    <div class="logo">
-        <img src="logo.jfif" alt="Logo">
-        <h1>MaternCare</h1>
-    </div>
-    
-    <ul>
-        <li><a href="adminhome.php">Home</a></li>
-        <li><a href="adminreport.php">Report</a></li>
-        <li><a href="doctorlist.php">Doctor List</a></li>
-        <li><a href="adminrecord.php">Record</a></li>
-        <li><a href="logout.php"class="logout-btn">Sign Out</a>
-    </li>
-    </ul>
+<div class="logo">
+<img src="logo.jfif">
+<h1>MaternCare</h1>
+</div>
+
+<ul>
+<li><a href="adminhome.php">Home</a></li>
+<li><a href="adminreport.php">Report</a></li>
+<li><a href="doctorlist.php">Doctor List</a></li>
+<li><a href="bookinglist.php">Booking List</a></li>
+<li><a href="adminrecord.php">Record</a></li>
+<li><a href="logout.php"class="logout-btn">Sign Out</a></li>
+</ul>
 
 </nav>
 
@@ -65,52 +114,78 @@ if(isset($_POST['add']))
 
 <div class="form-box">
 
-    <?php if($message != "") { ?>
-        <p class="message">
-            <?php echo $message; ?>
-        </p>
-    <?php } ?>
+<?php
+if($message!="")
+{
+?>
 
-    <div class="row">
-        <label>Doctor ID</label>
-        <input type="text" name="doctor_id" required>
-    </div>
+<p class="message">
+<?= $message ?>
+</p>
 
-    <div class="row">
-        <label>Name</label>
-        <input type="text" name="name" required>
-    </div>
+<?php
+}
+?>
 
-    <div class="row">
-        <label>Specialization</label>
+<div class="row">
 
-        <select name="specialization" required>
-            <option value="">Select</option>
-            <option>Obstetrician & Gynecologist (OB-GYN)</option>
-            <option>Pediatrician</option>
-            <option>Dietitian</option>
-            <option>Surgeon</option>
-        </select>
-    </div>
+<label>Doctor ID</label>
 
-    <div class="row">
-        <label>Email</label>
-        <input type="email" name="email" required>
-    </div>
+<input type="text" name="doctor_id" value="<?= $doctor_id ?>"readonly>
 
-    <div class="button-area">
-        <button type="submit" name="add">
-            Add Doctor
-        </button>
+</div>
 
-        <button type="reset">
-            Clear
-        </button>
-    </div>
+<div class="row">
+
+<label>Name</label>
+
+<input type="text" name="name"required>
+
+</div>
+
+<div class="row">
+
+<label>Specialization</label>
+
+<select name="specialization" required>
+
+<option value="">Select</option>
+
+<option>Obstetrician & Gynecologist (OB-GYN)</option>
+<option>Maternal-Fetal Medicine Specialist</option>
+<option>Family Medicine Specialist</option>
+<option>Dietitian / Nutritionist</option>
+<option>Anesthesiologist</option>
+<option>Midwife</option>
+<option>General Practitioner (GP)</option>
+
+</select>
+
+</select>
+
+</div>
+
+<div class="row">
+
+<label>Email</label>
+
+<input type="email" name="email" required>
+
+</div>
+
+<div class="button-area">
+
+<button type="submit"name="add">Add Doctor
+</button>
+
+<button type="reset"> Clear</button>
+
+</div>
 
 </div>
 
 </form>
 
 </body>
+
 </html>
