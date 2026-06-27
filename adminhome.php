cb28b6586fabaf74e051c4c30a7f3ba7ff8dabf9
zahirahfarah80-tech <zahirahfarah80@gmail.com>
2026-06-27 21:01:09 +0800

    Update adminhome.php

--- adminhome.php
+++ adminhome.php
@@ -39,6 +39,7 @@ $doctor = mysqli_num_rows($doctor_result);
         <li><a href="adminhome.php">Home</a></li>
         <li><a href="adminreport.php">Report</a></li>
         <li><a href="doctorlist.php">Doctor List</a></li>
+        <li><a href="bookinglist.php">Booking List</a></li>
         <li><a href="adminrecord.php">Record</a></li>
         <li><a href="logout.php"class="logout-btn">Sign Out</a>
     </li>
